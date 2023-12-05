d94cfad8e74e11a32c55d423f9a52d6703ff065f
sourabh verma <100554551+Sourabh16Verma@users.noreply.github.com>
2023-12-05 12:36:12 +0530

    Initial commit

---
+++ cou.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- for count function -->
+<?php
+ $car1 = array("volvo","alto","fortuner");
+ echo count($car1);  
+?> <br>
+<!-- for is_array function -->
+<?php
+$a = "sourabh";
+    echo "a is " . is_array($a);
+    $b = array("sdf","ghj");
+    echo "b is " . is_array($b);
+    ?><br>
+    <!--  substr function -->
+    <?php
+    echo substr("sourabh",3);
+    ?> <br>
+    <!--  in_array function -->
+    <?php
+  $people = array("sourabh","narendra","manish","rahul");
+  if(in_array("sourabh",$people,TRUE)){
+    echo "MATCH FOUND <br>";
+  }
+  else{
+    "MATCH NOT FOUND";
+  }
+    ?><br>
+    <!-- explode functions  -->
+    <?php
+    $str = "sourabh verma";
+    print_r (explode(" ", $str));
+    ?><br>
+    <!-- implode function -->
+    <?php 
+    $arr = array('hello','world','beautiful','D');
+     echo implode(" ", $arr);
+    ?><br>
+    <!--str replace function  -->
+    <?php
+    echo str_replace("verma", "peter","sourabh verma");
+    ?><br>
+    <!-- strlen function -->
+    <?php
+    echo strlen("sourabh");
+    ?><br>
+    <!-- array-merge -->
+    <?php
+    $arr1 = array("1","2");
+    $arr2 = array("3","4");
+    print_r(array_merge($arr1,$arr2));
+    ?><br>
+    <!-- strpos function -->
+    <?php
+    echo strpos("I love php", "o");
+    ?><br>
+    <!-- preg-match function -->
+    <?php
+    $str = "sourabh";
+    $pattern = "/verma/i";
+    echo preg_match($pattern,$str);
+    ?><br>
+    <!-- sprintf function -->
+    <?php 
+    $number = 22;
+    $str = "India";
+    $txt = sprintf("there are %u million bicycles in %s ",$number,$str);
+    echo $txt;
+    ?><br>
+    <!-- trim function -->
+    <?php
+    $str = "sourabh batki";
+    echo $str; "<br>";
+    echo trim($str, "oa");
+?>
+    
+</body>
+</html>
